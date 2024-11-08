eeab034aae7f7d822977132521579be4232b8464
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2024-11-08 11:27:19 +0100

    wip

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -10,18 +10,6 @@ class PirateIsland {
 
 
 describe("Pirate island", () => {
-    it("should be in the jail when starting out", () => {
-        const pirateIsland = new PirateIsland()
-        pirateIsland.move();
-        expect(pirateIsland.location).toEqual("Jail")
-    });
-
-    it("should be able to move to town", () => {
-        const pirateIsland = new PirateIsland()
-        pirateIsland.move("Town");
-
-        expect(pirateIsland.location).toEqual("Town");
-    })
 
     it.each([['Bar', "Town", "Jail"]])
     ("should be able to move to the bar", (location: Location) => {

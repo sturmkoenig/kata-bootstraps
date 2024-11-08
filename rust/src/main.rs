de7252e276e5c50f452572abe2fe320226d2cf2f
linus lauer <linus.lauer@codecentric.de>
2024-11-08 12:01:40 +0100

    wip

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,25 @@
+
+#[derive(Debug)]
+struct Action {
+}
+
+fn get_valid_actions(p0: (), p1: _) -> _ {
+    todo!()
+}
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_should_fail() {
-        assert_eq!(false, true);
-    }
+    use std::collections::HashSet;
+    use crate::{get_valid_actions, Action};
 
     #[test]
-    fn test_should_succeed() {
-        assert_eq!(true, true);
+    fn test_move_is_valid() {
+        let prison = ();
+        let node2 = ();
+        let state = ();
+
+        assert_eq!(get_valid_actions(prison, vec!["banana"]), vec![Action {}]);
     }
+
 }
+
+fn main() {}

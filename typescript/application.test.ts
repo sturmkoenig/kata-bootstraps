bd141c7c2ca4a6b5a69f2d7871a9fbee536776a1
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2024-11-08 11:27:00 +0100

    wip

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -1,34 +1,35 @@
 type Location = "Jail" | "Town" | "Bar";
 
 class PirateIsland {
-  location: Location;
+    location: Location;
 
-  move(location: Location = "Jail") {
-    this.location = location;
-  }
+    move(location: Location = "Jail") {
+        this.location = location;
+    }
 }
 
 
 describe("Pirate island", () => {
-  it("should be in the jail when starting out", () => {
-    const pirateIsland = new PirateIsland()
-    pirateIsland.move();
-    expect(pirateIsland.location).toEqual("Jail")
-  });
+    it("should be in the jail when starting out", () => {
+        const pirateIsland = new PirateIsland()
+        pirateIsland.move();
+        expect(pirateIsland.location).toEqual("Jail")
+    });
 
-  it("should be able to move to town", () => {
-    const pirateIsland = new PirateIsland()
-    pirateIsland.move("Town");
+    it("should be able to move to town", () => {
+        const pirateIsland = new PirateIsland()
+        pirateIsland.move("Town");
 
-    expect(pirateIsland.location).toEqual("Town");
-  })
+        expect(pirateIsland.location).toEqual("Town");
+    })
 
-  it.each([['Bar', "Town", "Jail"]]) ("should be able to move to the bar", (loaction: Location) => {
-    const pirateIsland = new PirateIsland()
-    pirateIsland.move(loaction);
+    it.each([['Bar', "Town", "Jail"]])
+    ("should be able to move to the bar", (location: Location) => {
+        const pirateIsland = new PirateIsland()
+        pirateIsland.move(location);
 
-    expect(pirateIsland.location).toEqual(loaction);
-  })
+        expect(pirateIsland.location).toEqual(location);
+    })
 
 
 });

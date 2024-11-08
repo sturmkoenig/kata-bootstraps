cbfc1d663d8e284d4f75f5cd7ae64b85cb755c82
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2024-11-08 11:10:21 +0100

    should be able to move to town

--- typescript/application.test.ts
+++ typescript/application.test.ts
@@ -1,11 +1,20 @@
-function getInitialLocation() {
+function getInitialLocation(location: string) {
+  if (location) {
+    return "town"
+  }
   return "Jail"
 }
 
-describe("A Test", () => {
+describe("Pirate island", () => {
   it("should be in the jail when starting out", () => {
-    const location = getInitialLocation();
+    const location = getInitialLocation("");
     expect(location).toEqual("Jail")
   });
+
+  it("should be able to move to town", () => {
+    const location = getInitialLocation("Town");
+
+    expect(location).toEqual("Town");
+  })
 
 });
